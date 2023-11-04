a651feb3bbd9a1b0efdf6a7bfa7e5323188dc3eb
Satapas T <satapastanachotpaisit@gmail.com>
2023-11-04 14:55:00 +0000

    Fixed some stuff

--- Data analysis/Data importer.py
+++ Data analysis/Data importer.py
@@ -7,27 +7,32 @@ import os
 os.system('cls')
 dataframe = pd.read_csv('data.csv')
 
-print(dataframe.to_string()) 
-print(dataframe.index)
-print(dataframe[['Pulse']])
+#print(dataframe.to_string()) 
+#print(dataframe.index)
+#print(dataframe[['Pulse']])
 
-def print_columns(name):
+def print_columns(dataframe,column_name):
     for current_data in range(len(dataframe)):
-        print(dataframe.loc[current_data, name])
+        print(dataframe.loc[current_data, column_name])
 
-def locate_item(name,item_target): 
-    for current_data in range(len(dataframe)):
-        if (dataframe.loc[current_data, name]) == item_target:
-            print(dataframe.loc[current_data])
+def locate_item(dataframe,column_name,item_target): 
+    for index, row in dataframe.iterrows():
+        if row[column_name] == item_target:
+            print(row)
 
-def datasize():
+def datasize(dataframe):
     count = 0
     for current_data in range(len(dataframe)):
         count += 1
     return count
 
-for i in range(10):
-    print("-------------------------------------------------------------------")
-locate_item('Pulse','90')
 
-print(datasize())
+print_columns(dataframe, 'Pulse')
+for i in range(3):
+    print("-------------------------------------------------------------------")
+
+locate_item(dataframe,'Pulse','90')
+
+for i in range(3):
+    print("-------------------------------------------------------------------")
+print(datasize(dataframe))
