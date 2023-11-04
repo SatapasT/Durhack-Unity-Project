dc994f76349dc04360541b0b1f6bdd5c1683d10d
Satapas T <satapastanachotpaisit@gmail.com>
2023-11-04 14:17:04 +0000

    a

--- Data analysis/Data importer.py
+++ Data analysis/Data importer.py
@@ -1,5 +1,24 @@
 import pandas as pd
+import numpy as np
+import os
 
-df = pd.read_csv('data.csv')
 
-print(df.to_string()) 
+
+os.system('cls||clear')
+dataframe = pd.read_csv('data.csv')
+
+print(dataframe.to_string()) 
+print(dataframe.index)
+print(dataframe[['Pulse']])
+
+def print_columns(name):
+    for current_data in range(dataframe.size):
+        print(dataframe.loc[current_data, name])
+
+def locate_item(name,item_target): 
+    for current_data in range(dataframe.size):
+        if (dataframe.loc[current_data, name]) == item_target:
+            print(dataframe.loc[current_data, name])
+
+print_columns('Pulse')
+#locate_item('Pulse','90')
