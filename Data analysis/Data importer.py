124960deaad1a9938d5bbb1611de7e83082c4860
Satapas T <satapastanachotpaisit@gmail.com>
2023-11-04 14:25:55 +0000

    Fix stuff

--- Data analysis/Data importer.py
+++ Data analysis/Data importer.py
@@ -4,7 +4,7 @@ import os
 
 
 
-os.system('cls||clear')
+os.system('cls')
 dataframe = pd.read_csv('data.csv')
 
 print(dataframe.to_string()) 
@@ -12,13 +12,22 @@ print(dataframe.index)
 print(dataframe[['Pulse']])
 
 def print_columns(name):
-    for current_data in range(dataframe.size):
+    for current_data in range(len(dataframe)):
         print(dataframe.loc[current_data, name])
 
 def locate_item(name,item_target): 
-    for current_data in range(dataframe.size):
+    for current_data in range(len(dataframe)):
         if (dataframe.loc[current_data, name]) == item_target:
-            print(dataframe.loc[current_data, name])
+            print(dataframe.loc[current_data])
 
-print_columns('Pulse')
-#locate_item('Pulse','90')
+def datasize():
+    count = 0
+    for current_data in range(len(dataframe)):
+        count += 1
+    return count
+
+for i in range(10):
+    print("-------------------------------------------------------------------")
+locate_item('Pulse','90')
+
+print(datasize())
